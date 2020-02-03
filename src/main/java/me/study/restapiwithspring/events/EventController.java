59a5ac06838973100072f70d5691a3b8e62a0c4c
Jeongsu Yang <mycat83@gmail.com>
2020-02-03 17:41:19 +0900

    이벤트 수정 API 구현

--- src/main/java/me/study/restapiwithspring/events/EventController.java
+++ src/main/java/me/study/restapiwithspring/events/EventController.java
@@ -79,6 +79,34 @@ public class EventController {
         return ResponseEntity.ok(eventResource);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<?>> updateEvent(@PathVariable Integer id,
+                                                      @RequestBody @Valid EventDto eventDto,
+                                                      Errors errors) {
+        Optional<Event> optionalEvent = this.eventRepository.findById(id);
+        if (optionalEvent.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (errors.hasErrors()) {
+            return badRequest(errors);
+        }
+
+        this.eventValidator.validate(eventDto, errors);
+        if (errors.hasErrors()) {
+            return badRequest(errors);
+        }
+
+        Event existingEvent = optionalEvent.get();
+        this.modelMapper.map(eventDto, existingEvent);
+        Event saveEvent = this.eventRepository.save(existingEvent);
+
+        EventResource eventResource = new EventResource(saveEvent);
+        eventResource.add(new Link("/docs/index.html#resources-events-update").withRel("profile"));
+
+        return ResponseEntity.ok(eventResource);
+    }
+
     private ResponseEntity<EntityModel<?>> badRequest(Errors errors) {
         return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
