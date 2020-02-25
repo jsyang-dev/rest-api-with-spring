1478027021322be6d00c81d95a50c4d0a6f56545
Jeongsu Yang <mycat83@gmail.com>
2020-02-25 17:42:44 +0900

    Account 도메인 추가

---
+++ src/main/java/me/study/restapiwithspring/accounts/AccountRole.java
@@ -0,0 +1,6 @@
+package me.study.restapiwithspring.accounts;
+
+public enum AccountRole {
+
+    ADMIN, USER
+}
--- src/main/java/me/study/restapiwithspring/events/Event.java
+++ src/main/java/me/study/restapiwithspring/events/Event.java
@@ -1,6 +1,7 @@
 package me.study.restapiwithspring.events;
 
 import lombok.*;
+import me.study.restapiwithspring.accounts.Account;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -30,6 +31,8 @@ public class Event {
     private boolean free;
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
+    @ManyToOne
+    private Account manager;
 
     public void update() {
         // Update free
