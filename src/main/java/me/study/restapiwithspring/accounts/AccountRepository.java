0529efc7fa99fdeb7b76f5cf1491bc72f73ffe33
Jeongsu Yang <mycat83@gmail.com>
2020-02-26 22:36:01 +0900

    스프링 시큐리티 적용

---
+++ src/main/java/me/study/restapiwithspring/accounts/AccountRepository.java
@@ -0,0 +1,9 @@
+package me.study.restapiwithspring.accounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findAllByEmail(String username);
+}
