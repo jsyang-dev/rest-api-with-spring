cf0105389f5e07754d763308b9533dc4cd91b4c1
Jeongsu Yang <mycat83@gmail.com>
2020-02-27 22:02:34 +0900

    예외 테스트

--- src/test/java/me/study/restapiwithspring/accounts/AccountServiceTest.java
+++ src/test/java/me/study/restapiwithspring/accounts/AccountServiceTest.java
@@ -1,23 +1,31 @@
 package me.study.restapiwithspring.accounts;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.hamcrest.Matchers.containsString;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -43,5 +51,34 @@ public class AccountServiceTest {
 
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void findByUsernameFail() {
+        String username = "random@gmail.com";
+        accountService.loadUserByUsername(username);
+    }
+
+    @Test
+    public void findByUsernameFail2() {
+        String username = "random@gmail.com";
+
+        try {
+            accountService.loadUserByUsername(username);
+            fail("Supposed to be failed");
+        } catch(UsernameNotFoundException e) {
+            assertThat(e.getMessage()).contains(username);
+        }
+    }
+
+    @Test
+    public void findByUsernameFail3() {
+        // Expected
+        String username = "random@gmail.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
     }
 }
