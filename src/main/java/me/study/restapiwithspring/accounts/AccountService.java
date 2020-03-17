00a51aae82479eb90124887c866a2ee8ee652de7
Jeongsu Yang <mycat83@gmail.com>
2020-03-17 22:42:48 +0900

    현재 사용자 조회

--- src/main/java/me/study/restapiwithspring/accounts/AccountService.java
+++ src/main/java/me/study/restapiwithspring/accounts/AccountService.java
@@ -3,7 +3,6 @@ package me.study.restapiwithspring.accounts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -32,7 +31,7 @@ public class AccountService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountRepository.findAllByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+        return new AccountAdapter(account);
     }
 
     private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
--- src/main/java/me/study/restapiwithspring/events/EventController.java
+++ src/main/java/me/study/restapiwithspring/events/EventController.java
@@ -1,5 +1,7 @@
 package me.study.restapiwithspring.events;
 
+import me.study.restapiwithspring.accounts.Account;
+import me.study.restapiwithspring.accounts.CurrentUser;
 import me.study.restapiwithspring.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -35,7 +38,9 @@ public class EventController {
     }
 
     @PostMapping
-    public ResponseEntity<EntityModel<?>> createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+    public ResponseEntity<EntityModel<?>> createEvent(@RequestBody @Valid EventDto eventDto,
+                                                      Errors errors,
+                                                      @CurrentUser Account account) {
         if (errors.hasErrors()) {
             return badRequest(errors);
         }
@@ -47,6 +52,7 @@ public class EventController {
 
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
+        event.setManager(account);
         Event newEvent = this.eventRepository.save(event);
 
         WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
@@ -60,15 +66,21 @@ public class EventController {
     }
 
     @GetMapping
-    public ResponseEntity<PagedModel<EntityModel<Event>>> queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+    public ResponseEntity<PagedModel<EntityModel<Event>>> queryEvents(Pageable pageable,
+                                                                      PagedResourcesAssembler<Event> assembler,
+                                                                      @CurrentUser Account account) {
         Page<Event> page = this.eventRepository.findAll(pageable);
         PagedModel<EntityModel<Event>> pagedModel = assembler.toModel(page, EventResource::new);
         pagedModel.add(new Link("/docs/index.html#resources-events-list").withRel("profile"));
+        if (account != null) {
+            pagedModel.add(linkTo(EventController.class).withRel("create-event"));
+        }
         return ResponseEntity.ok(pagedModel);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EntityModel<?>> getEvent(@PathVariable Integer id) {
+    public ResponseEntity<EntityModel<?>> getEvent(@PathVariable Integer id,
+                                                   @CurrentUser Account account) {
         Optional<Event> optionalEvent = this.eventRepository.findById(id);
         if (optionalEvent.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -76,13 +88,17 @@ public class EventController {
         Event event = optionalEvent.get();
         EventResource eventResource = new EventResource(event);
         eventResource.add(new Link("/docs/index.html#resources-events-get").withRel("profile"));
+        if (event.getManager().equals(account)) {
+            eventResource.add(linkTo(EventController.class).slash(event.getId()).withRel("update-event"));
+        }
         return ResponseEntity.ok(eventResource);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EntityModel<?>> updateEvent(@PathVariable Integer id,
                                                       @RequestBody @Valid EventDto eventDto,
-                                                      Errors errors) {
+                                                      Errors errors,
+                                                      @CurrentUser Account account) {
         Optional<Event> optionalEvent = this.eventRepository.findById(id);
         if (optionalEvent.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -98,6 +114,10 @@ public class EventController {
         }
 
         Event existingEvent = optionalEvent.get();
+        if (!existingEvent.getManager().equals(account)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
         this.modelMapper.map(eventDto, existingEvent);
         Event saveEvent = this.eventRepository.save(existingEvent);
 
